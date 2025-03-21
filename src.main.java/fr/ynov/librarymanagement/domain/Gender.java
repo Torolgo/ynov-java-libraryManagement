8d08d706678b11d2c8a10a2922c0957dbcbb69cf
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-21 14:41:19 +0100

    feat(Gender): add Gender enum to categorize book genres

---
+++ src.main.java/fr/ynov/librarymanagement/domain/Gender.java
@@ -0,0 +1,15 @@
+package fr.ynov.librarymanagement.domain;
+
+public enum Gender {
+    FICTION,
+    NON_FICTION,
+    SCIENCE_FICTION,
+    FANTASY,
+    MYSTERY,
+    HORROR,
+    ROMANCE,
+    THRILLER,
+    BIOGRAPHY,
+    HISTORY,
+    COOKBOOK,
+}
