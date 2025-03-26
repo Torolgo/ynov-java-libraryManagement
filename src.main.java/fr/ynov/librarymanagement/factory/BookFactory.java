ae92d6d7b433bb0e57bfa9c7f6e1e2c0fe828d4c
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-26 19:04:16 +0100

    feat(BookFactory, PersonFactory): add methods to clear lists and update biographies, improve file reading logic

--- src.main.java/fr/ynov/librarymanagement/factory/BookFactory.java
+++ src.main.java/fr/ynov/librarymanagement/factory/BookFactory.java
@@ -1,6 +1,5 @@
 package fr.ynov.librarymanagement.factory;
 
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import fr.ynov.librarymanagement.domain.*;
 import com.google.gson.Gson;
@@ -8,42 +7,62 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookFactory {
     private static final ArrayList<Book> bookList = new ArrayList<Book>();
 
+    public static void clearBookList() {
+        bookList.clear();
+    }
+
     public static void ReadNovelsFile() {
         Gson gson = new Gson();
         List<Novel> novelJson = null;
         try {
-            novelJson = gson.fromJson(new FileReader("src.main.java/fr/ynov/librarymanagement/assets/novels.json"), new TypeToken<List<Novel>>() {
-            }.getType());
-            bookList.addAll(novelJson);
+            File file = new File("src.main.java/fr/ynov/librarymanagement/assets/novels.json");
+            if (file.exists() && file.length() > 0) {
+                novelJson = gson.fromJson(new FileReader(file), new TypeToken<List<Novel>>() {
+                }.getType());
+                if (novelJson != null) {
+                    bookList.addAll(novelJson);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public static void ReadBdsFile() {
         Gson gson = new Gson();
         List<Bd> bdJson = null;
         try {
-            bdJson = gson.fromJson(new FileReader("src.main.java/fr/ynov/librarymanagement/assets/bd.json"), new TypeToken<List<Bd>>() {
-            }.getType());
-            bookList.addAll(bdJson);
+            File file = new File("src.main.java/fr/ynov/librarymanagement/assets/bd.json");
+            if (file.exists() && file.length() > 0) {
+                bdJson = gson.fromJson(new FileReader(file), new TypeToken<List<Bd>>() {
+                }.getType());
+                if (bdJson != null) {
+                    bookList.addAll(bdJson);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public static void ReadMangasFile() {
         Gson gson = new Gson();
         List<Manga> mangaJson = null;
         try {
-            mangaJson = gson.fromJson(new FileReader("src.main.java/fr/ynov/librarymanagement/assets/manga.json"), new TypeToken<List<Manga>>() {
-            }.getType());
-            bookList.addAll(mangaJson);
+            File file = new File("src.main.java/fr/ynov/librarymanagement/assets/manga.json");
+            if (file.exists() && file.length() > 0) {
+                mangaJson = gson.fromJson(new FileReader(file), new TypeToken<List<Manga>>() {
+                }.getType());
+                if (mangaJson != null) {
+                    bookList.addAll(mangaJson);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,13 +71,15 @@ public class BookFactory {
     public static void WriteNovelsFile(int id, String title, Author author, Genre genre, int year, int pages, int chapters) {
         Gson gson = new Gson();
         File file = new File("src.main.java/fr/ynov/librarymanagement/assets/novels.json");
-        List<Novel> novelJson = null;
+        List<Novel> novelJson = new ArrayList<>();
         try {
             if (file.exists() && file.length() > 0) {
                 novelJson = gson.fromJson(new FileReader(file), new TypeToken<List<Novel>>() {
                 }.getType());
             }
-            assert novelJson != null;
+            if (novelJson == null) {
+                novelJson = new ArrayList<>();
+            }
             novelJson.forEach(novel -> {
                 if (novel.getId() == id) {
                     throw new IllegalArgumentException("This id already exists");
@@ -72,16 +93,19 @@ public class BookFactory {
             e.printStackTrace();
         }
     }
+
     public static void WriteBdsFile(int id, String title, Author author, Genre genre, int year, int pages, Illustrator illustrator, String illustrationStyle) {
         Gson gson = new Gson();
         File file = new File("src.main.java/fr/ynov/librarymanagement/assets/bd.json");
-        List<Bd> bdJson = null;
+        List<Bd> bdJson = new ArrayList<>();
         try {
             if (file.exists() && file.length() > 0) {
                 bdJson = gson.fromJson(new FileReader(file), new TypeToken<List<Bd>>() {
                 }.getType());
             }
-            assert bdJson != null;
+            if (bdJson == null) {
+                bdJson = new ArrayList<>();
+            }
             bdJson.forEach(bd -> {
                 if (bd.getId() == id) {
                     throw new IllegalArgumentException("This id already exists");
@@ -95,16 +119,19 @@ public class BookFactory {
             e.printStackTrace();
         }
     }
+
     public static void WriteMangasFile(int id, String title, Author author, Genre genre, int year, int pages, String subGender) {
         Gson gson = new Gson();
         File file = new File("src.main.java/fr/ynov/librarymanagement/assets/manga.json");
-        List<Manga> mangaJson = null;
+        List<Manga> mangaJson = new ArrayList<>();
         try {
             if (file.exists() && file.length() > 0) {
                 mangaJson = gson.fromJson(new FileReader(file), new TypeToken<List<Manga>>() {
                 }.getType());
             }
-            assert mangaJson != null;
+            if (mangaJson == null) {
+                mangaJson = new ArrayList<>();
+            }
             mangaJson.forEach(manga -> {
                 if (manga.getId() == id) {
                     throw new IllegalArgumentException("This id already exists");
@@ -115,6 +142,109 @@ public class BookFactory {
             fileWriter.write(gson.toJson(mangaJson));
             fileWriter.close();
         }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateBookStatus(Book book) {
+        if (book instanceof Novel) {
+            updateNovelStatus((Novel) book);
+        } else if (book instanceof Bd) {
+            updateBdStatus((Bd) book);
+        } else if (book instanceof Manga) {
+            updateMangaStatus((Manga) book);
+        }
+    }
+
+    private static void updateNovelStatus(Novel novel) {
+        Gson gson = new Gson();
+        File file = new File("src.main.java/fr/ynov/librarymanagement/assets/novels.json");
+        List<Novel> novelJson = new ArrayList<>();
+        try {
+            if (file.exists() && file.length() > 0) {
+                novelJson = gson.fromJson(new FileReader(file), new TypeToken<List<Novel>>() {
+                }.getType());
+            }
+            if (novelJson == null) {
+                return;
+            }
+
+            for (Novel n : novelJson) {
+                if (n.getId() == novel.getId()) {
+                    n.takeBook();  // Will be set to true/false based on actual status
+                    if (!novel.isTaken()) {
+                        n.returnBook();
+                    }
+                    break;
+                }
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(novelJson));
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateBdStatus(Bd bd) {
+        Gson gson = new Gson();
+        File file = new File("src.main.java/fr/ynov/librarymanagement/assets/bd.json");
+        List<Bd> bdJson = new ArrayList<>();
+        try {
+            if (file.exists() && file.length() > 0) {
+                bdJson = gson.fromJson(new FileReader(file), new TypeToken<List<Bd>>() {
+                }.getType());
+            }
+            if (bdJson == null) {
+                return;
+            }
+
+            for (Bd b : bdJson) {
+                if (b.getId() == bd.getId()) {
+                    b.takeBook();  // Will be set to true
+                    if (!bd.isTaken()) {
+                        b.returnBook();
+                    }
+                    break;
+                }
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(bdJson));
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateMangaStatus(Manga manga) {
+        Gson gson = new Gson();
+        File file = new File("src.main.java/fr/ynov/librarymanagement/assets/manga.json");
+        List<Manga> mangaJson = new ArrayList<>();
+        try {
+            if (file.exists() && file.length() > 0) {
+                mangaJson = gson.fromJson(new FileReader(file), new TypeToken<List<Manga>>() {
+                }.getType());
+            }
+            if (mangaJson == null) {
+                return;
+            }
+
+            for (Manga m : mangaJson) {
+                if (m.getId() == manga.getId()) {
+                    m.takeBook();  // Will be set to true
+                    if (!manga.isTaken()) {
+                        m.returnBook();
+                    }
+                    break;
+                }
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(mangaJson));
+            fileWriter.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src.main.java/fr/ynov/librarymanagement/factory/PersonFactory.java
+++ src.main.java/fr/ynov/librarymanagement/factory/PersonFactory.java
@@ -11,76 +11,152 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonFactory {
-    private static ArrayList<Person> PersonList = new ArrayList<Person>();
+    private static final ArrayList<Person> PersonList = new ArrayList<>();
+
+    public static void clearPersonList() {
+        PersonList.clear();
+    }
 
     public static void ReadAuthorFile() {
         Gson gson = new Gson();
-        List<Person> authorList = null;
+        List<Person> authorList;
         try {
             authorList = gson.fromJson(new FileReader("src.main.java/fr/ynov/librarymanagement/assets/authors.json"), new TypeToken<List<Author>>() {
             }.getType());
-            PersonList.addAll(authorList);
+            if (authorList != null) {
+                PersonList.addAll(authorList);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
+
     public static void ReadIllustratorFile() {
         Gson gson = new Gson();
-        List<Person> illustratorList = null;
+        List<Person> illustratorList;
         try {
             illustratorList = gson.fromJson(new FileReader("src.main.java/fr/ynov/librarymanagement/assets/illustrators.json"), new TypeToken<List<Illustrator>>() {
             }.getType());
-            PersonList.addAll(illustratorList);
+            if (illustratorList != null) {
+                PersonList.addAll(illustratorList);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 
     public static void WriteAuthorFile(int id, String name, String surname, String nationality, String dateOfBirth, String Biography, String wrintingStyle) {
         Gson gson = new Gson();
         File file = new File("src.main.java/fr/ynov/librarymanagement/assets/authors.json");
-        List<Author> authorJson = null;
+        List<Author> authorJson = new ArrayList<>();
         try {
             if (file.exists() && file.length() > 0) {
                 authorJson = gson.fromJson(new FileReader(file), new TypeToken<List<Author>>() {
                 }.getType());
             }
-            assert authorJson != null;
-            authorJson.forEach(author -> {
+            if (authorJson == null) {
+                authorJson = new ArrayList<>();
+            }
+            for (Author author : authorJson) {
                 if (author.getId() == id) {
                     throw new IllegalArgumentException("This id already exists");
                 }
-            });
+            }
             authorJson.add(new Author(id, name, surname, nationality, dateOfBirth, Biography, wrintingStyle));
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(gson.toJson(authorJson));
             fileWriter.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public static void WriteIllustratorFile(int id, String name, String surname, String nationality, String dateOfBirth, String Biography, String illustrationStyle) {
         Gson gson = new Gson();
         File file = new File("src.main.java/fr/ynov/librarymanagement/assets/illustrators.json");
-        List<Illustrator> illustratorJson = null;
+        List<Illustrator> illustratorJson = new ArrayList<>();
         try {
             if (file.exists() && file.length() > 0) {
                 illustratorJson = gson.fromJson(new FileReader(file), new TypeToken<List<Illustrator>>() {
                 }.getType());
             }
-            assert illustratorJson != null;
-            illustratorJson.forEach(illustrator -> {
+            if (illustratorJson == null) {
+                illustratorJson = new ArrayList<>();
+            }
+            for (Illustrator illustrator : illustratorJson) {
                 if (illustrator.getId() == id) {
                     throw new IllegalArgumentException("This id already exists");
                 }
-            });
+            }
             illustratorJson.add(new Illustrator(id, name, surname, nationality, dateOfBirth, Biography, illustrationStyle));
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(gson.toJson(illustratorJson));
             fileWriter.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateAuthorBiography(int id, String newBiography) {
+        Gson gson = new Gson();
+        File file = new File("src.main.java/fr/ynov/librarymanagement/assets/authors.json");
+        List<Author> authorJson = new ArrayList<>();
+        try {
+            if (file.exists() && file.length() > 0) {
+                authorJson = gson.fromJson(new FileReader(file), new TypeToken<List<Author>>() {
+                }.getType());
+            }
+            if (authorJson == null) {
+                return;
+            }
+
+            boolean found = false;
+            for (Author author : authorJson) {
+                if (author.getId() == id) {
+                    author.setBiography(newBiography);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(gson.toJson(authorJson));
+                fileWriter.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateIllustratorBiography(int id, String newBiography) {
+        Gson gson = new Gson();
+        File file = new File("src.main.java/fr/ynov/librarymanagement/assets/illustrators.json");
+        List<Illustrator> illustratorJson = new ArrayList<>();
+        try {
+            if (file.exists() && file.length() > 0) {
+                illustratorJson = gson.fromJson(new FileReader(file), new TypeToken<List<Illustrator>>() {
+                }.getType());
+            }
+            if (illustratorJson == null) {
+                return;
+            }
+
+            boolean found = false;
+            for (Illustrator illustrator : illustratorJson) {
+                if (illustrator.getId() == id) {
+                    illustrator.setBiography(newBiography);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(gson.toJson(illustratorJson));
+                fileWriter.close();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
